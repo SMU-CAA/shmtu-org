e280b9d423259380f0dee856f6612c22ac5a02e5
Liam Chan <imbytecat@gmail.com>
2020-12-11 00:13:14 +0800

    Create dns_records.js

---
+++ dns_records.js
@@ -0,0 +1,22 @@
+const dns_records = [
+    {
+        name: "caa",
+        type: "A",
+        value: "1.1.1.1"
+    },
+    {
+        name: "sts",
+        type: "A",
+        value: "10.64.154.2"
+    },
+    {
+        name: "www",
+        type: "A",
+        value: "1.1.1.1"
+    },
+    {
+        name: "@",
+        type: "A",
+        value: "1.1.1.1"
+    },
+]
